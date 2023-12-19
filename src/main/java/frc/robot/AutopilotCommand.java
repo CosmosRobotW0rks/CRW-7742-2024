af2b647b7ae4358483cf4b4ad6e2dfb137240296
feoranis26 <alpdoganer26@gmail.com>
2023-12-19 22:28:47 +0300

    AutopilotCommand now engages the autopilot

--- src/main/java/frc/robot/AutopilotCommand.java
+++ src/main/java/frc/robot/AutopilotCommand.java
@@ -25,7 +25,7 @@ public class AutopilotCommand extends CommandBase{
 
     @Override
     public void end(boolean interrupted) {
-        Driver.Goto(null);
+        Driver.Disengage();
     }
 
     @Override
