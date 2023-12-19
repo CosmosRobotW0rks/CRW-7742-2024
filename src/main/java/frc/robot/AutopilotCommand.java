88579519cf63d523a83704f22d3b0c58c68b2281
feoranis26 <alpdoganer26@gmail.com>
2023-12-19 22:17:17 +0300

    remove unused imports

--- src/main/java/frc/robot/AutopilotCommand.java
+++ src/main/java/frc/robot/AutopilotCommand.java
@@ -1,9 +1,7 @@
 package frc.robot;
 
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.AutopilotDriver;
 
 public class AutopilotCommand extends CommandBase{
 
