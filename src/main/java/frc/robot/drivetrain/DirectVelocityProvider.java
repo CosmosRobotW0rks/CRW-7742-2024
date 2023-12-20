88b855b28efaa334ec7963366fcdd17ff0dfa951
feoranis26 <alpdoganer26@gmail.com>
2023-12-20 12:25:09 +0300

    fix velocity provider default null value

--- src/main/java/frc/robot/drivetrain/DirectVelocityProvider.java
+++ src/main/java/frc/robot/drivetrain/DirectVelocityProvider.java
@@ -3,7 +3,7 @@ package frc.robot.drivetrain;
 import edu.wpi.first.math.geometry.Translation3d;
 
 public class DirectVelocityProvider extends VelocityProvider {
-    Translation3d vel;
+    Translation3d vel = new Translation3d(0, 0, 0);
     public void SetVelocity(Translation3d velocity){
         vel = velocity;
     }
