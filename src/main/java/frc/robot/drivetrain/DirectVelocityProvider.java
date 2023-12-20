1b2c71965c3d7ef4c0a1a2aa118165d5820629bf
feoranis26 <alpdoganer26@gmail.com>
2023-12-20 12:34:12 +0300

    formatting

--- src/main/java/frc/robot/drivetrain/DirectVelocityProvider.java
+++ src/main/java/frc/robot/drivetrain/DirectVelocityProvider.java
@@ -4,7 +4,8 @@ import edu.wpi.first.math.geometry.Translation3d;
 
 public class DirectVelocityProvider extends VelocityProvider {
     Translation3d vel = new Translation3d(0, 0, 0);
-    public void SetVelocity(Translation3d velocity){
+
+    public void SetVelocity(Translation3d velocity) {
         vel = velocity;
     }
 
