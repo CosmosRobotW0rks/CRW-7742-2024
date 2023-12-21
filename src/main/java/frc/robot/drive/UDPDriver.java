f0814071b2d71b36283ff1208cd52e91244713c7
feoranis26 <alpdoganer26@gmail.com>
2023-12-21 21:18:00 +0300

    add UDP driver

---
+++ src/main/java/frc/robot/drive/UDPDriver.java
@@ -0,0 +1,24 @@
+package frc.robot.drive;
+
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotContainer;
+import frc.robot.drivetrain.DirectVelocityProvider;
+
+public class UDPDriver extends SubsystemBase {
+    private DirectVelocityProvider vp = new DirectVelocityProvider();
+    private DatagramSocket vel_sock;
+    private DatagramSocket gyro_sock;
+
+    public UDPDriver(RobotContainer c) throws SocketException {
+        vel_sock = new DatagramSocket(11752);
+        gyro_sock = new DatagramSocket(11752);
+    }
+
+    @Override
+    public void periodic() {
+
+    }
+}
